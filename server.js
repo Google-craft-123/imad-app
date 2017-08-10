91ecf0a151381dc8d72bb0a47127657086744aa7
Google-craft-123 <30626947+Google-craft-123@users.noreply.github.com>
2017-08-10 11:11:23 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
-app.get('/counter'function (req,res){
-    counter = counter + 1;
-    res.send(counter.toString());
-});
-
 var articles = {
     'article-one':{
     title:'Article One|Sai Ganesha',
@@ -90,6 +84,14 @@ function createTemplate (data) {
 `;
     return htmlTemplate;
 }
+
+
+var counter = 0;
+app.get('/counter', function (req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 
 
 app.get('/:articleName', function(req,res){

694295c66a61a486ed77765d99853258bde1dd74
Google-craft-123 <30626947+Google-craft-123@users.noreply.github.com>
2017-08-10 17:08:08 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,16 @@ app.get('/counter', function (req,res){
     res.send(counter.toString());
 });
 
-
+var names = [];
+app.get('/submit-name', function(req,res) {//URL: submit-name?name=xxxxxxx
+ //Get the name from the request
+ var name = req.query.name;
+ 
+ names.push(name);
+ //JSON: Javascript Object Notation!
+ 
+ res.send(JSON.stringify(names));
+});
 
 app.get('/:articleName', function(req,res){
     var articleName = req.params.articleName;
@@ -114,16 +123,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-var names = [];
-app.get('/submit-name', function(req,res) {//URL: submit-name?name=xxxxxxx
- //Get the name from the request
- var name = req.query.name;
- 
- names.push(name);
- //JSON: Javascript Object Notation!
- 
- res.send(JSON.stringify(names));
-});
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers

2e058c3e97c7f7d39123ef134931a6c8bcdc7e91
Google-craft-123 <30626947+Google-craft-123@users.noreply.github.com>
2017-08-28 20:46:37 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config = {
+    user: 'sganesha75',
+    database: 'sganesha75',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
 
 var articles = {
     'article-one':{
@@ -81,7 +85,23 @@ var articles = {
     </div>`
     }
 };
+
+var pool = new Pool(config);
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});    
     
+app.get('/test-db', function (req, res){
+    //make a select request
+    //return a response with the results
+    pool.query('SELECT * FROM test', function (err, result){
+        if (err) {
+         res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));   
+        }
+    });
+});
     
 function createTemplate (data) {
     var title = data.title;
